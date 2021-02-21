f36d77cae60a1052b0acc962149636f5ba7f66d8
Victor Lin <linvictorw@gmail.com>
2021-02-21 18:33:24 +0800

    Updates

---
+++ precache-manifest.dae887bcaf2c2a13941ad2026ee2fcd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e5160c1a23d4af0cec22dda87edd52",
+    "url": "/seasons-hooks/index.html"
+  },
+  {
+    "revision": "95f20b90c1259e02ebe4",
+    "url": "/seasons-hooks/static/css/main.7839f605.chunk.css"
+  },
+  {
+    "revision": "2ad27b4941654774f07e",
+    "url": "/seasons-hooks/static/js/2.b308d9f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons-hooks/static/js/2.b308d9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f20b90c1259e02ebe4",
+    "url": "/seasons-hooks/static/js/main.cd6dbb34.chunk.js"
+  },
+  {
+    "revision": "bd33d3b8699d79696115",
+    "url": "/seasons-hooks/static/js/runtime-main.523612cb.js"
+  }
+]);
