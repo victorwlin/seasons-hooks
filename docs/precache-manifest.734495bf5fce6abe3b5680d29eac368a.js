2cb3ba5f1892b11467da34742efd8ce6d2e8ed0f
Victor Lin <linvictorw@gmail.com>
2021-02-21 18:27:17 +0800

    build

---
+++ docs/precache-manifest.734495bf5fce6abe3b5680d29eac368a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949a24c3025c6cf3c86a30e0b93724d0",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "95f20b90c1259e02ebe4",
+    "url": "/seasons/static/css/main.7839f605.chunk.css"
+  },
+  {
+    "revision": "2ad27b4941654774f07e",
+    "url": "/seasons/static/js/2.b308d9f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.b308d9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f20b90c1259e02ebe4",
+    "url": "/seasons/static/js/main.cd6dbb34.chunk.js"
+  },
+  {
+    "revision": "c9aa463c2c8102aff538",
+    "url": "/seasons/static/js/runtime-main.d04c89a8.js"
+  }
+]);
